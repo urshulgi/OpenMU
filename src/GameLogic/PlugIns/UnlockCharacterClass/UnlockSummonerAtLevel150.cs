5a7e8fb118bc5b9615b9f7e431f6759057bc26df
Nick Baldallo <nnickbaldallo@gmail.com>
2024-05-13 22:49:56 +0100

    Test new plugin - SummonerAtLevel150

---
+++ src/GameLogic/PlugIns/UnlockCharacterClass/UnlockSummonerAtLevel150.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="UnlockSummonerAtLevel150.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.PlugIns.UnlockCharacterClass;
+
+using System.Runtime.InteropServices;
+using MUnique.OpenMU.PlugIns;
+
+/// <summary>
+/// Unlocks the Summoner character class as soon as the first character of an account reaches level 150.
+/// </summary>
+[PlugIn(nameof(UnlockSummonerAtLevel150), "Unlocks the Summoner character class as soon as the first character of an account reaches level 150.")]
+[Guid("D25D109F-99BC-4816-B680-530332B417EC")]
+public class UnlockSummonerAtLevel150 : UnlockCharacterAtLevelBase
+{
+    private const byte SummonerNumber = 20;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UnlockSummonerAtLevel150"/> class.
+    /// </summary>
+    public UnlockSummonerAtLevel150()
+        : base(SummonerNumber, 150, "Summoner")
+    {
+    }
+}
